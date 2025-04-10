ed4a256f5c9b741f1b84457625eae8524e136b81
Vilayat Gajar <gajarovv2002@gmail.com>
2025-04-10 10:14:01 -0400

    completed Mod3 Exercise 2 - MathApp

---
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class MathApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+   /*     System.out.println("Enter Bob's salary: ");
+        double bobSalary = sc.nextDouble();
+        System.out.println("Enter Gary's: ");
+        double garySalary = sc.nextDouble();
+      double bobSalary = 12000;
+      double garySalary = 10000;
+      double highestSalary = Math.max(bobSalary, garySalary);
+        String name;
+        if (highestSalary == bobSalary){
+            name = "Bob";
+        }
+        else {
+            name = "Gary";
+        }
+        System.out.println(name +" has higher salary:" + highestSalary + "$");
+        //Exercise 3
+        double pi = 3.14159;
+        double radius = 7.25;
+        double area = pi * radius * radius;
+        System.out.println("The area of the circle is: " + area);
+*/
+     //Exercise 4
+    /*    System.out.println("Enter square radius: ");
+        double radius = sc.nextDouble();
+        double squareRoot = Math.sqrt(radius);
+        System.out.printf("Square root is: %.2f", squareRoot);
+
+        //Exercise 5
+        System.out.println("Enter x1: ");
+        double x1 = sc.nextDouble();
+        System.out.println("Enter x2: ");
+        double x2 = sc.nextDouble();
+        System.out.println("Enter y1: ");
+        double y1 = sc.nextDouble();
+        System.out.println("Enter y2: ");
+        double y2 = sc.nextDouble();
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.printf("Distance is: %.2f", distance);
+    */
+        //Exercise 6
+  /*      System.out.println("Enter your decimal number: ");
+        double decimal = sc.nextDouble();
+        double absolute = Math.abs(decimal);
+        System.out.printf("Absolute value is: %.2f", absolute);
+   */
+        //Exercise 7
+        System.out.println("Your random number is: " + Math.random());
+        
+
+    }
+}
